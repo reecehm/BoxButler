2169a323c57c957092f910927a1d9558094de1e2
reecehm <reecehmartin@gmail.com>
2024-03-03 16:15:47 -0600

    Added tabView to alternate between main view and "search" and "settings" views

---
+++ BoxButler/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  BoxButler
+//
+//  Created by 64014784 on 3/3/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
---
+++ BoxButler/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  BoxButler
+//
+//  Created by 64014784 on 3/3/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
