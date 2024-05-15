34cd8b36309e5f156d62c02b9affcb4a18999b85
luke-perket <64001174@ep-student.org>
2024-05-15 11:37:12 -0500

    kind of broken filter

---
+++ BoxButler/LocationTagSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationTagSelectionView.swift
+//  BoxButler
+//
+//  Created by 64001174 on 5/13/24.
+//
+
+import SwiftUI
+
+struct LocationTagSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationTagSelectionView()
+}
